a1b78bab049717eca8a763d9f46a63db519c979b
MohamedEssam <mo849026@gmail.com>
2023-11-11 02:49:36 +0200

    fit and evaluate the linear regression model to predict the loan amount

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,4 @@
+import classifier as classifier
 import pandas
 import numpy as np
 import seaborn as sns
@@ -5,6 +6,8 @@ import matplotlib.pyplot as plot
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import r2_score
+
 
 #Load the "loan_old.csv" dataset.
 data = pandas.read_csv('loan_old.csv')
@@ -25,7 +28,7 @@ print('----------------------')
 
 #visualize a pairplot between numercial columns
 sns.pairplot(data[['Income','Coapplicant_Income','Credit_History','Loan_Tenor','Max_Loan_Amount']])
-plot.show()
+#plot.show()
 
 
 
@@ -66,17 +69,22 @@ for column_name in data_cleaned_rows.columns:
 x=data_cleaned_rows.drop(columns=['Max_Loan_Amount','Loan_Status'])
 y=data_cleaned_rows[['Max_Loan_Amount','Loan_Status']]
 
+
 #the data is shuffled and split into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
+y_train_max_loan = y_train['Max_Loan_Amount']
+y_test_max_loan = y_test['Max_Loan_Amount']
+y_train_loan_status = y_train['Loan_Status']
+y_test_loan_status = y_test['Loan_Status']
+
 
 #numerical features are standardized
 cnt=0
 for column_name in x_train.columns:
     if data_types.iloc[cnt] == 'int64' or data_types.iloc[cnt] == 'float64':
         x_train[column_name]=(x_train[column_name]-x_train[column_name].mean())/x_train[column_name].std()
-        x_test[column_name] = (x_train[column_name] - x_train[column_name].mean()) / x_train[column_name].std()
+        x_test[column_name] = (x_test[column_name] - x_test[column_name].mean()) / x_test[column_name].std()
     cnt += 1
-
 
 # y_train['Max_Loan_Amount'] = (y_train['Max_Loan_Amount'] - y_train['Max_Loan_Amount'].mean()) / y_train['Max_Loan_Amount'].std()
 # y_test['Max_Loan_Amount'] = (y_train['Max_Loan_Amount'] - y_train['Max_Loan_Amount'].mean()) / y_train['Max_Loan_Amount'].std()
@@ -85,11 +93,22 @@ for column_name in x_train.columns:
 #Convert data to Numpy array
 x_train = x_train.to_numpy().reshape((-1,9))
 x_test = x_test.to_numpy().reshape((-1,9))
-y_train = y_train.to_numpy()
-y_test = y_test.to_numpy()
+y_train_max_loan = y_train_max_loan.to_numpy()
+y_test_max_loan = y_test_max_loan.to_numpy()
 
 
+#Fit a linear regression model
+model = linear_model.LinearRegression()
+model.fit(x_train,y_train_max_loan)
 
+print('Coefficients: \n', model.coef_, " ", model.intercept_)
+
+
+#predict the loan amount
+y_pred = model.predict(x_test)
+
+r2 = r2_score(y_test_max_loan, y_pred)
+print("R-squared score:", r2)
 
 
 
